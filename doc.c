9d9219eb0632fd2a4d7535cb10ab288bd51d0f5e
Juan <juanpablodejesus_figueroa@ucol.mx>
2020-02-20 12:28:11 -0600

    Programa para leer documentos

---
+++ doc.c
@@ -0,0 +1,26 @@
+//Ej5_Funciones.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char*argv[]){
+    FILE *fp;
+    int i=0;
+    char c;
+    fp=fopen(argv[1],"r");
+    while(!feof(fp)){
+        c = getc(fp);
+        i++;
+    }
+    rewind(fp);
+    char leida[i];
+    while(fgets(leida, i, fp)) {
+        printf("%s",leida);
+    }
+
+    //fgets(leida,i,fp);
+    fclose(fp);
+    //printf("\nCadena leida: %s\n",leida);
+    //system("PAUSE");
+    return 0;
+}
+
